9fc599a33a716321d422df9153b52d15c90e4c25
Shiva-Kandagatla98 <sxk52450@ucmo.edu>
2023-11-26 19:49:22 -0600

    push

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ from project import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    application.run(debug=True) 
+    application.run(debug=True,host="127.0.0.1", port=5000)
